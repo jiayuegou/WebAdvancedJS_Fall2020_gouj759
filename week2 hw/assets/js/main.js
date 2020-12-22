ee67ca6efe716afb7a62c66cdbd6199dcdd29068
Glitch (cclab-portfolio-jiayue) <none>
2020-12-22 04:36:12 +0000

    🐃👛 Updated with Glitch

--- week2 hw/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-widthHeight = () => {
-    document.getElementById("widthheight").innerHTML = ("W: " + window.innerWidth + " --- H: " + window.innerHeight);
-};
-
-window.onresize = widthHeight;
-window.onload = widthHeight;
